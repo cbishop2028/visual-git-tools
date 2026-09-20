9385a979a45e968ef4994592280d8d62f8a71ce7
cbishop <connorbishop0303@gmail.com>
2026-09-20 16:40:34 -0400

    Change prices to account for a diet coke.

--- checkout.py
+++ checkout.py
@@ -4,5 +4,5 @@ def calculate_total(items, tax_rate=0.05):                 # function the price 
     return total * 0.8
 
 if __name__ == "__main__":
-    prices = [10.00, 25.50, 4.25]                           # array to store prices in
+    prices = [10.00, 25.50, 4.25, 3.00]                           # array to store prices in
     print(f"Total: ${calculate_total(prices):.2f}")
