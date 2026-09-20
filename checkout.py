5ade955c6fcd45041005d1d2d9fc1ebab4e1bff3
cbishop <connorbishop0303@gmail.com>
2026-09-20 16:22:25 -0400

    Change ckecout.py to multiply total by 0.8

--- checkout.py
+++ checkout.py
@@ -1,7 +1,7 @@
 def calculate_total(items, tax_rate=0.05):                 # function the price of groceries with tax
     subtotal = sum(items)                                  # sum function
     total = subtotal + (subtotal * tax_rate)
-    return total * 0.5
+    return total * 0.8
 
 if __name__ == "__main__":
     prices = [10.00, 25.50, 4.25]                           # array to store prices in
